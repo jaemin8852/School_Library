d37f44bef92b3ace0be627f4a73e807515dbf3bc
Jubil <jaemin8852@naver.com>
2018-09-06 13:20:05 +0900

    Implement crawling school code

--- app/src/main/java/jubil/com/schoollibrary/MainActivity.java
+++ app/src/main/java/jubil/com/schoollibrary/MainActivity.java
@@ -1,13 +1,33 @@
 package jubil.com.schoollibrary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button search_btn;
+    Button reset_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        search_btn = findViewById(R.id.search_btn);
+        reset_btn = findViewById(R.id.reset_btn);
+
+        search_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Search
+                //http://reading.ssem.or.kr/r/reading/search/schoolCodeSetting.jsp?schoolCode=31051&returnUrl=
+
+                Intent intent=new Intent(MainActivity.this,WebViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/jubil/com/schoollibrary/WebViewActivity.java
@@ -0,0 +1,78 @@
+package jubil.com.schoollibrary;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    WebView wv;
+    String url = "http://reading.ssem.or.kr/r/reading/search/schoolCodeSetting.jsp?schoolCode=31051&returnUrl=";
+    Handler mHandler;
+    Button btn;
+    int current_code= -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.web_view);
+
+        wv = findViewById(R.id.web_view);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl(url);
+        wv.setWebViewClient(new WebViewClientClass());
+
+        btn = findViewById(R.id.btn);
+
+        mHandler = new Handler();
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //여기서 x
+                mHandler.post(new Runnable () {
+
+                    @Override
+                    public void run() {
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Log.d("Url", wv.getUrl());
+                                wv.reload();
+                                Log.d("Url", wv.getUrl());
+                                wv.setWebViewClient(new WebViewClientClass());
+                            }
+                        });
+                    }
+                });
+            }
+        });
+        t.start();
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            Log.d("", "getUrl : " + view.getUrl());
+            Log.d("", "url : " + url);
+            if(url.contains("schoolCode")) current_code = Integer.parseInt(url.substring(url.indexOf('?')+12, url.indexOf('&')));
+            Log.d("code = ", Integer.toString(current_code));
+        }
+
+    }
+}
